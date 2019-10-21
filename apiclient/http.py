fcee736187436ca47acae853208f6560f85c5a2e
Khusbu Mishra <meetkhusbumishra@gmail.com>
2019-10-21 12:47:49 +0530

    Pass strings instead of tuple in urlunparse, add idea to gitignore

--- apiclient/http.py
+++ apiclient/http.py
@@ -704,9 +704,8 @@ class HttpRequest(object):
       self.headers['content-type'] = 'application/x-www-form-urlencoded'
       parsed = urllib.parse.urlparse(self.uri)
       self.uri = urllib.parse.urlunparse(
-          (parsed.scheme, parsed.netloc, parsed.path, parsed.params, '',
-           '')
-          )
+        parsed.scheme, parsed.netloc, parsed.path, parsed.params, '', ''
+      )
       self.body = parsed.query
       self.headers['content-length'] = str(len(self.body))
 
@@ -1088,8 +1087,8 @@ class BatchHttpRequest(object):
     # Construct status line
     parsed = urllib.parse.urlparse(request.uri)
     request_line = urllib.parse.urlunparse(
-        ('', '', parsed.path, parsed.params, parsed.query, '')
-        )
+      '', '', parsed.path, parsed.params, parsed.query, ''
+    )
     status_line = request.method + ' ' + request_line + ' HTTP/1.1\n'
     major, minor = request.headers.get('content-type', 'application/json').split('/')
     msg = MIMENonMultipart(major, minor)
